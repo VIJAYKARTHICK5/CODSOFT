1964b4c766e2bf05e689265a58c0fe224bbbe773
VIJAYKARTHICK5 <vijayakarthickraju@gmail.com>
2024-07-09 18:57:11 +0530

    Create TASK 5-Student.java

---
+++ TASK 5-Student.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class Student {
+    private String studentID;
+    private String name;
+    private ArrayList<Course> registeredCourses;
+
+    public Student(String studentID, String name) {
+        this.studentID = studentID;
+        this.name = name;
+        this.registeredCourses = new ArrayList<>();
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Course> getRegisteredCourses() {
+        return registeredCourses;
+    }
+
+    public boolean registerCourse(Course course) {
+        if (course.addStudent(this)) {
+            registeredCourses.add(course);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean dropCourse(Course course) {
+        if (registeredCourses.remove(course)) {
+            course.removeStudent(this);
+            return true;
+        }
+        return false;
+    }
+}
